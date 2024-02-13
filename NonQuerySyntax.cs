d18cc170b6e16f24291c5753d24171523284dc34
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-02-13 02:40:44 +0200

    Briefly

---
+++ NonQuerySyntax.cs
@@ -0,0 +1,22 @@
+﻿namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Names = new List<string>()
+            {
+                "Red","Book","Computer","Mobile","Program",
+                "Driver","Window","Key","Home","Pen","Right","Play",
+                "Clic","Language","Rule","Peace","Word","File"
+            };
+            // Non Query Syntax (Fluent Syntax)
+            var Result = Names.Where(item=>item.Contains("R")).Take(2);
+            
+            foreach (var item in Result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
