cd7e101bd37fae6e86b95571ab33dde3bd29152f
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2024-02-25 01:33:41 +0200

    Count,Sum,Max,Min,Average

---
+++ AggregateFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+namespace join
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 4, 5, 7, 8, 2 };
+            // Count => Count How many number in array
+            Console.WriteLine(numbers.Count());
+
+            // Sum => Sum numbers in array
+            Console.WriteLine(numbers.Sum());
+
+            // Max => Get maximum number in array
+            Console.WriteLine(numbers.Max());
+
+            // Min => Get minimum number in array
+            Console.WriteLine(numbers.Min());
+
+            // Count => Get Average of numbers in array
+            Console.WriteLine(numbers.Average());
+
+        }
+    }
+
+}
